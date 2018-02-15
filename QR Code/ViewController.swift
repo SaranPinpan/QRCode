ae4666fef53b246f69e1fd12bcd93b5c0bbea64a
Saran Pinpan <saran.pi@hotmail.com>
2018-02-15 16:02:54 +0700

    Initial Commit

---
+++ QR Code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QR Code
+//
+//  Created by Saran Pinpan on 15/2/18.
+//  Copyright © 2018 Saran Pinpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
